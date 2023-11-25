b84cd67fd3bcd6b8a6ea5207b1d02194bf1f1a24
wacek44 <wacek44.ww@gmail.com>
2023-11-25 16:47:42 +0100

    Initial commit

---
+++ src/main/java/pl/strefakursow/eLunchApp/config/JPAConfiguration.java
@@ -0,0 +1,66 @@
+package pl.strefakursow.eLunchApp.config;
+
+import com.google.common.collect.ImmutableMap;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.HibernateExceptionTranslator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories("pl.strefakursow.eLunchApp.repo")
+@EnableTransactionManagement
+public class JPAConfiguration {
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/eLunchApp?useUnicode=true&characterEncoding=utf8&" +
+                "useSSL=false&serverTimezone=UTC§allowPublicKeyRetrieval=true");
+        dataSourceBuilder.username("samuel");
+        dataSourceBuilder.password("15042017");
+        return dataSourceBuilder.build();
+    }
+    @Bean
+    public HibernateJpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        return adapter;
+    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, HibernateJpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter);
+        entityManagerFactory.setPackagesToScan("pl.strefakursow.eLunchApp.model");
+        entityManagerFactory.setJpaPropertyMap(ImmutableMap.of(
+                AvailableSettings.DIALECT, "org.hibernate.dialect.MySQL8Dialect",
+                AvailableSettings.SHOW_SQL, "true",
+                AvailableSettings.HBM2DDL_AUTO, "create"
+        ));
+        return entityManagerFactory;
+    }
+    @Bean
+    public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.createEntityManager();
+    }
+    @Bean
+    public HibernateExceptionTranslator hibernateExceptionTranslator() {
+        return new HibernateExceptionTranslator();
+    }
+    @Bean(name = "transaktionManager")
+    public PlatformTransactionManager platformTransactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
---
+++ src/test/java/pl/strefakursow/eLunchApp/ELunchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.strefakursow.eLunchApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELunchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
